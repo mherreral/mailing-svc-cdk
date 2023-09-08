9a5bfe0c776a62707fa1d64b394ab3ab3fb4793b
mherreral <mherlop77@gmail.com>
2023-09-08 10:49:42 -0500

    moved pipeline to main app

--- bin/mailing-svc.ts
+++ bin/mailing-svc.ts
@@ -3,8 +3,11 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { LambdaStack } from '../lib/lambda-stack';
 import { BucketStack } from '../lib/s3-bucket-stack';
+import { MailingSvcPipelineStack } from '../lib/mailing-svc-pipeline-stack';
+
 
 const app = new cdk.App();
 
 const bucketStack = new BucketStack(app, 'BucketStack', {})
 new LambdaStack(app, 'LambdaStack', {bucket: bucketStack.bucket})
+new MailingSvcPipelineStack(app, 'MyPipelineStack', {});
